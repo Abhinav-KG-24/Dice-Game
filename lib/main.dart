e395a31ac722bfbac3b9e5e454d773733fc03cae
abhinav kg <abhinavkg24@gmail.com>
2023-11-15 21:31:05 +0530

    Removed the debug banner , and finished the project

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    backgroundColor: Colors.green.shade900,
-    appBar: AppBar(
-      backgroundColor: Colors.green.shade900,
-    ),
-    body: const DicePage(),
-  )));
+  runApp(MaterialApp(home: DicePage(), debugShowCheckedModeBanner: false));
 }
 
 class DicePage extends StatefulWidget {
@@ -23,38 +16,44 @@ class _DicePageState extends State<DicePage> {
   var diceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 150,
-            height: 150,
-            child: TextButton(
-              style: TextButton.styleFrom(foregroundColor: Colors.green[900]),
-              onPressed: () {
-                setState(() {
-                  diceNumber = Random().nextInt(6) + 1;
-                });
-              },
-              child: Image.asset('Assets/images/Dice$diceNumber.jpg'),
+    return Scaffold(
+      backgroundColor: Colors.green.shade900,
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade900,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              child: TextButton(
+                style: TextButton.styleFrom(foregroundColor: Colors.green[900]),
+                onPressed: () {
+                  setState(() {
+                    diceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('Assets/images/Dice$diceNumber.jpg'),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 100,
-          ),
-          Container(
-            height: 100,
-          ),
-          Text(
-            'Dice Number : $diceNumber',
-            style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontFamily: 'Agbalumo'),
-          ),
-        ],
+            const SizedBox(
+              height: 100,
+            ),
+            Container(
+              height: 100,
+            ),
+            Text(
+              'Dice Number : $diceNumber',
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: 'Agbalumo'),
+            ),
+          ],
+        ),
       ),
     );
   }
